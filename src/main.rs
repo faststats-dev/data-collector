f54d14602ef7baf220b5c971b59198d365d82b09
Luca <lucameopou@gmail.com>
2026-01-22 11:56:34 +0100

    fix: add accidentally removed RequestDecompressionLayer

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::signal;
 use tower_http::cors::{AllowOrigin, CorsLayer};
+use tower_http::decompression::RequestDecompressionLayer;
 mod batch_queue;
 mod handler;
 mod models;
@@ -92,6 +93,7 @@ async fn main() {
         .route("/v1/web/metadata", get(handler::web_metadata))
         .route("/v1/vitals", post(handler::vitals))
         .route("/v1/replay", post(handler::replay))
+        .layer(RequestDecompressionLayer::new())
         .layer(cors)
         .with_state(state);
 
